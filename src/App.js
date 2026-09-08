30276d0eca62201ec6a3d3c5bc8a881988660a7c
nosdvp <mishchenkoa.97@gmail.com>
2026-09-08 10:54:38 +0300

    Added full responsive layout for mobile, tablet, and desktop Implemented date validation based on the number of days in each month Added leap year calculation Added validation for months with 30 and 31 days Implemented displaying calculated data only after button click

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import img from './img/icon-arrow.svg'
 
 function App() {
 
-  const [day, setDay] = useState('')
-  const [month, setMonth] = useState('')
-  const [year, setYear] = useState('')
+  const [inputDay, setInputDay] = useState('')
+  const [confirmDay, setconfirmDay] = useState('')
+
+  const [inputMonth, setInputMonth] = useState('')
+  const [confirmMonth, setConfirmMonth] = useState('')
+
+  const [inputYear, setInputYear] = useState('')
+  const [confirmYear, setConfirmYear] = useState('')
 
   const date = new Date
 
@@ -14,69 +19,88 @@ function App() {
   const getMonth = date.getMonth() //месяц
   const getYear = date.getFullYear() //
 
-  console.log(getYear)
+  const calcDate = () => {
+    setconfirmDay(inputDay)
+    setConfirmMonth(inputMonth)
+    setConfirmYear(inputYear)
+  } 
+
+  let countDayInMonth
+
+  if(inputMonth === '2' && inputYear % 4 === 0){
+    countDayInMonth = 29
+  }else if(inputMonth === '2'){
+    countDayInMonth = 28
+  }else if(inputMonth === '4' || 
+           inputMonth === '6' || 
+           inputMonth === '9' ||
+           inputMonth === '11'){
+    countDayInMonth = 30
+  }else{
+    countDayInMonth = 31
+  }
 
   return (
     <div className="wrapper">
       <main className='boxContent'>
         <section className='boxContent__enterData'>
           <div className='boxContent__enterData_day'>
-            {day > 31 ? <p className='boxContent__enterData_day_error'>DAY</p> : <p className='boxContent__enterData_day_correct'>DAY</p>}
+            {inputDay > countDayInMonth ? <p className='boxContent__enterData_day_error'>DAY</p> : <p className='boxContent__enterData_day_correct'>DAY</p>}
             <input
-              className={day > 31 ? 'boxContent__enterData_day_inputError' : 'boxContent__enterData_day_inputCorrect'}
+              className={inputDay > countDayInMonth ? 'boxContent__enterData_day_inputError' : 'boxContent__enterData_day_inputCorrect'}
               placeholder='DD'
               type='number'
-              value={day}
-              onChange={(e) => setDay(e.target.value)}
+              value={inputDay}
+              onChange={(e) => setInputDay(e.target.value)}
             />
-            {day > 31 ? <p>Must be a valid day</p> : <p></p>}
+            {inputDay > countDayInMonth ? <p>Must be a valid day</p> : <p></p>}
           </div>
 
           <div className='boxContent__enterData_month'>
-            {month > 12 ? <p className='boxContent__enterData_month_error'>MONTH</p> : <p className='boxContent__enterData_month_correct'>MONTH</p>}
+            {inputMonth > 12 ? <p className='boxContent__enterData_month_error'>MONTH</p> : <p className='boxContent__enterData_month_correct'>MONTH</p>}
             <input
-              className={month > 12 ? 'boxContent__enterData_month_inputError' : 'boxContent__enterData_month_inputCorrect'}
+              className={inputMonth > 12 ? 'boxContent__enterData_month_inputError' : 'boxContent__enterData_month_inputCorrect'}
               placeholder='MM'
               type='number'
-              value={month}
-              onChange={(e) => setMonth(e.target.value)}
+              value={inputMonth}
+              onChange={(e) => setInputMonth(e.target.value)}
             />
-            {month > 12 ? <p>Must be a valid month</p> : <p></p>}
+            {inputMonth > 12 ? <p>Must be a valid month</p> : <p></p>}
           </div>
 
           <div className='boxContent__enterData_year'>
-            {getYear < year ? <p className='boxContent__enterData_year_error'>YEAR</p> : <p className='boxContent__enterData_year_correct'>YEAR</p>}
+            {getYear < inputYear ? <p className='boxContent__enterData_year_error'>YEAR</p> : <p className='boxContent__enterData_year_correct'>YEAR</p>}
             <input
-              className={getYear < year ? 'boxContent__enterData_year_inputError' : 'boxContent__enterData_year_inputCorrect'}
+              className={getYear < inputYear ? 'boxContent__enterData_year_inputError' : 'boxContent__enterData_year_inputCorrect'}
               placeholder='YY'
               type='number'
-              value={year}
-              onChange={(e) => setYear(e.target.value)}
+              value={inputYear}
+              onChange={(e) => setInputYear(e.target.value)}
             />
-            {getYear < year ? <p>Must be in the past</p> : <p></p>}
+            {getYear < inputYear ? <p>Must be in the past</p> : <p></p>}
           </div>
         </section>
 
         <section className='line'>
           <div className='line__lineBlock'></div>
-          <button className='line__button'>
+          <button className='line__button' onClick={calcDate}>
             <img src={img}/>
           </button>
         </section>
 
         <section className='showData'>
           <div className='showData__years'>
-            {year === '' ? <p>--</p> : <p>{year}</p>}
+            {confirmYear === '' ? <p>--</p> : <p>{confirmYear}</p>}
             <p>years</p>
           </div>
 
           <div className='showData__months'>
-            {month === '' ? <p>--</p> : <p>{month}</p>}
+            {confirmMonth === '' ? <p>--</p> : <p>{confirmMonth}</p>}
             <p>months</p>
           </div>
 
           <div className='showData__days'>
-            {day === '' ? <p>--</p> : <p>{day}</p>}
+            {confirmDay === '' ? <p>--</p> : <p>{confirmDay}</p>}
             <p>days</p>
           </div>
         </section>
